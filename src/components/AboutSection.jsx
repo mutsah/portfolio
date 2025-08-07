b7cf8c371818809132d62f4029b0f33f28b715de
Mutsah <taringamutsah@gmail.com>
2025-08-07 12:31:36 +0200

    CV downloading

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -26,7 +26,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href="#contact"
+                href="/assets/docs/Mutsa_Taringa_CV.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
